925d3ff6b253017d38867b658819c25321dab9e6
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2022-05-31 13:20:57 +0900

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require( 'http' );
+const port = 80;
+
+const server = http.CreateServer( function( request, response ) {
+    response.writeHead( 200, { 'Content-Type':'text/html; charset=utr-8' } );
+    response.end( 'Hello, Node.js!!!' );
+} );
+
+server.listen( port, function() {
+    console.log( `Server is running...[ ${port} ]` );
+} );
