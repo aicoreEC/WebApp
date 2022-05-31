bccc34797cf69851b84efacd55fe2ad80f107e67
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2022-05-31 18:11:00 +0900

    modify server_request.js

--- server_request.js
+++ server_request.js
@@ -20,8 +20,8 @@ const server = http.createServer( function ( request, response ) {
         response.writeHead( 200, { 'Content-Type':'text/html; charset=utf-8' } );
         response.end( '홍길동' );
     } else {
-        response.writeHead( 200, { 'Content-Type':'text/html; charset=utf-8' } );
-        response.end( '404 Page Not Found' );
+        response.writeHead( 404, { 'Content-Type':'text/html; charset=utf-8' } );
+        //response.end( '404 Page Not Found' );
     }
 } ).listen( port, function() {
     console.log( `Server is running... [ ${port} ]` );
