f877fc43a389bf4c8cc37d91a031f93a72380e82
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2022-05-31 16:56:55 +0900

    modify server_request_post.js

---
+++ server_request_post.js
@@ -0,0 +1,22 @@
+const http = require( 'http' );
+const querystring = require( 'querystring' );
+
+const port = 8080;
+
+const server = http.createServer( function( request, response ) {
+    var postdata = '';
+
+    request.on( 'data', function( data ) {
+        postdata += data;
+    } );
+
+    request.on( 'end', function() {
+        const parsedQuery = querystring.parse( postdata );
+        console.log( parsedQuery );
+
+        response.writeHead( 200, { 'Content-Type':'test/html; charset=utf-8' } );
+        response.end( `var1 = [ ${parsedQuery.var1} ]` );
+    } )
+} ).listen( port, function() {
+    console.log( `Server is running... [ ${port} ]` );
+} );
