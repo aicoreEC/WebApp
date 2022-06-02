4a63d3fd6fe6683b9c3937f9761f7f57272f67c3
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2022-06-02 17:09:10 +0900

    modify app.js, create list.ejs

--- board/app.js
+++ board/app.js
@@ -6,8 +6,23 @@ const db_config = require( __dirname + '/config/database.js' );
 const conn = db_config.init();
 db_config.connect( conn );
 
+const bodyParser = require( 'body-parser' );
+app.set( 'views', __dirname + '/views' )
+app.set( 'view engine', 'ejs' );
+
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoder( { extended: false } ) );
+
 app.get( '/', function ( req, res ) {
     res.send( 'ROOT' );
+} );
+
+app.get( '/list', function ( req, res ) {
+    const sql = 'SELECT * FROM db_test.board';
+    conn.query( sql, function ( err, rows, fields ) {
+        if ( err ) { console.log( `query is not excuted. select fail...\n${err}` ); }
+        else { res.render( 'list.ejs', { list : rows } ); }
+    } )
 } );
 
 app.listen( port, () => console.log( `Server is running... [ ${port} ]` ) );
