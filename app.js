2ef74efda4e76e0efe23a44023b0ee786e78c5e5
aicoreEC <51801164+aicoreEC@users.noreply.github.com>
2022-06-02 11:21:18 +0900

    modify app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let app = express();
 
 let port = 8080;
 
-app.get( '/', ( req, res ) => res.send( 'Hello, Node.js!!!' ) );
+app.get( '/', ( req, res ) => res.send( 'Hello, Node.js!!! - GET method' ) );
+
+app.post( '/', ( req, res ) => res.send( 'Hello, Node.js!!! - POST method' ))
 
 app.listen( port, () => console.log( `Server is running... [ ${port} ]` ) );
